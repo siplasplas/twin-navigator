98caaeab8abfd175ce80cfcb12c3b25fc5fb89ec
Andrzej Borucki <borucki.andrzej@gmail.com>
2023-08-02 14:13:55 +0200

    focusInEvent knows panel side

--- PanelWidget.cpp
+++ PanelWidget.cpp
@@ -6,10 +6,9 @@ void PanelWidget::focusInEvent(QFocusEvent* event)
 {
     MainWindow* mainWindow = dynamic_cast<MainWindow*>(mainWidget);
     if (mainWindow) {
-        auto parent = parentWidget()->parentWidget()->parentWidget();
-        if (parent==mainWindow->panels[0])
+        if (this==mainWindow->panels[0])
             mainWindow->focusedPanel = 0;
-        else if (parent==mainWindow->panels[1])
+        else if (this==mainWindow->panels[1])
             mainWindow->focusedPanel = 1;
         else
             mainWindow->focusedPanel = -1;
